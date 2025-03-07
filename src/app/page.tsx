bf19c424df101c5936257caf30b563ab4c2cea07
TirthPShah <cptirthp.shah@gmail.com>
2025-03-07 23:49:46 +0530

    Added Google Auth But Styling is yet to be done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-import Image from "next/image";
+"use client"; // Make this a Client Component
+
+import { signOut, signIn, useSession } from "next-auth/react";
 import StatusCard from "@/components/StatusCard";
+
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
-    <StatusCard name="Mumbai Stationary" status="Online" pages={3} />
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      {session ? (
+        <div>
+          <h1>Welcome, {session.user?.name}</h1>
+          <button
+            className="mt-4 px-6 py-2 bg-red-500 text-white rounded-md"
+            onClick={() => signOut()}
+          >
+            Sign Out
+          </button>
+        </div>
+      ) : (
+        <button
+          className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md"
+          onClick={() => signIn("google")}
+        >
+          Sign In with Google
+        </button>
+      )}
+      <StatusCard name="Mumbai Stationary" status="Online" pages={3} />
+    </main>
   );
 }
