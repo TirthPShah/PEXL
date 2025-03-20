b842314e368fee7dbab1b8cd31b0dfb92899d330
Tirth P Shah <cptirthp.shah@gmail.com>
2025-03-20 15:18:33 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/pexl-favico.ico" />
+      </head>
       <body>{children}</body>
     </html>
   );
